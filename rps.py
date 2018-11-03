864f591aaa9d35b8fb87f2c0d5e641e91a89ca01
Mrakéta <kerriane@seznam.cz>
2018-11-03 14:26:01 +0100

    Predtim nez delam dep. injection

--- rps.py
+++ rps.py
@@ -1,17 +1,51 @@
 import random
 
-human = input('rock, paper of scissors? ')
-
-while human not in ['rock', 'paper', 'scissors']:
-    human = input('rock, paper of scissors?')
+def input_human_play():
+    play = input('rock, paper or scissors? ')
+    while not is_valid_play(play):
+        play = input('rock, paper or scissors? ')
+    return play
     
-computer = random.choice(['rock', 'paper', 'scissors'])
+def is_valid_play(play):
+    return play in ['rock', 'paper', 'scissors']
 
-print('Computer plays: ', computer)
+def generate_computer_play():
+    return random.choice(['rock', 'paper', 'scissors'])
 
-if human == computer:
-    print('it\'s a tie!')
-elif (human == 'rock' and computer == 'scissors') or (human == 'scissors' and computer == 'paper') or (human == 'paper' and computer == 'rock') :
-    print('Human wins!')
-elif (human == 'rock' and computer == 'paper') or (human == 'scissors' and computer == 'rock') or (human == 'paper' and computer == 'scissors') :
-	print('Computer wins!')
+def evaluate_game(human, computer):
+    if human == computer:
+        return 'tie'
+    elif (human == 'rock' and computer == 'scissors') or (human == 'scissors' and computer == 'paper') or (human == 'paper' and computer == 'rock') :
+        return 'human'
+    elif (human == 'rock' and computer == 'paper') or (human == 'scissors' and computer == 'rock') or (human == 'paper' and computer == 'scissors') :
+        return 'computer'
+
+# ------
+
+def main():
+    human = input_human_play()
+    computer = generate_computer_play()
+
+    print('Computer plays: ', computer)
+    game = evaluate_game(human, computer)
+    if game == 'tie':
+        print ('it\'s a tie!')
+    else:
+        print(f'{game} won')
+        
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+
+
+    # if human == computer:
+        # print('it\'s a tie!')
+    # elif (human == 'rock' and computer == 'scissors') or (human == 'scissors' and computer == 'paper') or (human == 'paper' and computer == 'rock') :
+        # print('Human wins!')
+    # elif (human == 'rock' and computer == 'paper') or (human == 'scissors' and computer == 'rock') or (human == 'paper' and computer == 'scissors') :
+        # print('Computer wins!')
